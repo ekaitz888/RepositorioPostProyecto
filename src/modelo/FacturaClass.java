12f363f90753d44e9a69cc00c1c0dd887011cd79
Gusmii <agusmanop@gmail.com>
2019-05-06 13:57:57 +0200

    Modelos de factura cambiados

--- src/modelo/FacturaClass.java
+++ src/modelo/FacturaClass.java
@@ -3,5 +3,46 @@ package modelo;
 public class FacturaClass extends Connect {
 protected int id_factura;
 protected int id_linea;
-protected int total;
+protected double total;
+
+public FacturaClass() {
+	super();
+	// TODO Auto-generated constructor stub
+}
+
+public FacturaClass(int id_factura, int id_linea, double total) {
+	super();
+	this.id_factura = id_factura;
+	this.id_linea = id_linea;
+	this.total = total;
+}
+
+public int getId_factura() {
+	return id_factura;
+}
+
+public void setId_factura(int id_factura) {
+	this.id_factura = id_factura;
+}
+
+public int getId_linea() {
+	return id_linea;
+}
+
+public void setId_linea(int id_linea) {
+	this.id_linea = id_linea;
+}
+
+public double getTotal() {
+	return total;
+}
+
+public void setTotal(double total) {
+	this.total = total;
+}
+
+
+
+
+
 }
--- src/modelo/FacturaModel.java
+++ src/modelo/FacturaModel.java
@@ -1,5 +1,56 @@
 package modelo;
 
-public class FacturaModel {
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
+public class FacturaModel extends FacturaClass {
+
+	ArrayList<FacturaModel> list = new ArrayList<FacturaModel>();
+
+	
+	
+	public FacturaModel(ArrayList<FacturaModel> list) {
+		super();
+		this.list = list;
+	}
+
+	public FacturaModel() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public FacturaModel(int id_factura, int id_linea, double total) {
+		super(id_factura, id_linea, total);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ArrayList<FacturaModel> getList() {
+		return list;
+	}
+
+	public void setList(ArrayList<FacturaModel> list) {
+		this.list = list;
+	}
+	
+	public void LoadData() throws SQLException {
+		
+		this.CreateConection();
+		
+		Statement st = this.con.createStatement();
+		ResultSet rs = st.executeQuery("SELECT * FROM `producto`");
+
+		while (rs.next()) { // reads the table line by line
+
+			FacturaModel myFactura = new FacturaModel();
+			myFactura.setId_factura(Integer.parseInt(rs.getString(1)));
+			myFactura.setId_linea(Integer.parseInt(rs.getString(2)));
+			myFactura.setTotal(Double.parseDouble(rs.getString(3)));
+			
+			this.list.add(myFactura);
+		}
+		this.disconnect();
+	}
+	
 }
