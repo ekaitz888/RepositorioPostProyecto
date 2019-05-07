67ed38aa624dc2fb2a22df90f04a80f089c45a93
ekaitz888 <ekaitzg@ymail.com>
2019-05-07 10:05:23 +0200

    Json de marca

--- src/controlador/CJson.java
+++ src/controlador/CJson.java
@@ -44,18 +44,39 @@ public class CJson extends HttpServlet {
 			e.printStackTrace();
 		}
 		//START JSON
- 		ArrayList<JSONObject> jsonArray=new ArrayList<JSONObject>();
+ 		ArrayList<JSONObject> jsonArrayProducto=new ArrayList<JSONObject>();
  	 	
  		for (int i=0;i<myProduct.getList().size();i++){
  			
- 			JSONObject jsonObject=new JSONObject();
- 			jsonObject.put("nombre", myProduct.getList().get(i).getNombre());
- 			jsonObject.put("precio", myProduct.getList().get(i).getPrecio());
- 			jsonObject.put("id_filtro", myProduct.getList().get(i).getId_filtro());
- 			jsonObject.put("imagen", myProduct.getList().get(i).getImagen());
+ 			JSONObject jsonObjectProducto=new JSONObject();
+ 			jsonObjectProducto.put("nombre", myProduct.getList().get(i).getNombre());
+ 			jsonObjectProducto.put("precio", myProduct.getList().get(i).getPrecio());
+ 			jsonObjectProducto.put("id_filtro", myProduct.getList().get(i).getId_filtro());
+ 			jsonObjectProducto.put("imagen", myProduct.getList().get(i).getImagen());
  			
- 			jsonArray.add(jsonObject);	
+ 			jsonArrayProducto.add(jsonObjectProducto);	
  		}
+ 		
+ 		MarcaModel myMarca = new MarcaModel();
+ 		
+ 		try {
+			myMarca.LoadData();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+ 		
+ 		ArrayList<JSONObject> jsonArrayMarca=new ArrayList<JSONObject>();
+ 		
+ 		for (int i=0; i<myMarca.getList().size();i++) {
+ 			JSONObject jsonObjectMarca=new JSONObject();
+ 			jsonObjectMarca.put("nombre", myMarca.getList().get(i).getNombre());
+ 			jsonArrayMarca.add(jsonObjectMarca);
+ 		}
+ 		
+ 		ArrayList<JSONObject> jsonArray=new ArrayList<JSONObject>();
+ 		jsonArray.addAll(jsonArrayProducto);
+ 		jsonArray.addAll(jsonArrayMarca);
 
  		response.getWriter().write(jsonArray.toString());	
  		System.out.print(jsonArray.toString());
