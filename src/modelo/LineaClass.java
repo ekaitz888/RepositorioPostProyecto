3138e68dacedc0ca87d3c229555e182d6bde3934
Gusmii <agusmanop@gmail.com>
2019-05-07 09:34:03 +0200

    Lineas modelo

---
+++ src/modelo/LineaClass.java
@@ -0,0 +1,37 @@
+package modelo;
+
+public class LineaClass extends Connect{
+
+	protected int id_linea;
+	protected int id_producto;
+	protected double precio;
+	public LineaClass() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public LineaClass(int id_linea, int id_producto, double precio) {
+		super();
+		this.id_linea = id_linea;
+		this.id_producto = id_producto;
+		this.precio = precio;
+	}
+	public int getId_linea() {
+		return id_linea;
+	}
+	public void setId_linea(int id_linea) {
+		this.id_linea = id_linea;
+	}
+	public int getId_producto() {
+		return id_producto;
+	}
+	public void setId_producto(int id_producto) {
+		this.id_producto = id_producto;
+	}
+	public double getPrecio() {
+		return precio;
+	}
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+	
+}
---
+++ src/modelo/LineaModel.java
@@ -0,0 +1,52 @@
+package modelo;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class LineaModel extends LineaClass{
+	ArrayList<LineaModel> list = new ArrayList<LineaModel>();
+
+	public LineaModel() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public LineaModel(int id_linea, int id_producto, double precio) {
+		super(id_linea, id_producto, precio);
+		// TODO Auto-generated constructor stub
+	}
+
+	public LineaModel(ArrayList<LineaModel> list) {
+		super();
+		this.list = list;
+	}
+
+	public ArrayList<LineaModel> getList() {
+		return list;
+	}
+
+	public void setList(ArrayList<LineaModel> list) {
+		this.list = list;
+	}
+	
+	public void LoadData() throws SQLException {
+		
+		this.CreateConection();
+		
+		Statement st = this.con.createStatement();
+		ResultSet rs = st.executeQuery("SELECT * FROM `producto`");
+
+		while (rs.next()) { // reads the table line by line
+
+			LineaModel myLinea = new LineaModel();
+			myLinea.setId_linea(Integer.parseInt(rs.getString(1)));
+			myLinea.setId_producto(Integer.parseInt(rs.getString(2)));
+			myLinea.setPrecio(Double.parseDouble(rs.getString(3)));
+			
+			this.list.add(myLinea);
+		}
+		this.disconnect();
+	}
+}
